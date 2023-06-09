ae4019db5e0f516ed4c095e92780752121dbbad9
Nicholas Talbot <talbotnic26@gmail.com>
2023-06-09 12:07:33 -0600

    working but will probably modify some css at some point

---
+++ src/Components/ActiveWord/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import MyAudioPlayer from '../MyAudioPlayer'
+import { Link } from 'react-router-dom'
+import './ActiveWord.css'
+
+interface ActiveWordProps {
+  starter: string,
+  definitions: string[],
+  synonyms: string[],
+  mp3Url: string
+}
+
+export default function ActiveWord( { starter, definitions, synonyms, mp3Url }: ActiveWordProps ): React.ReactElement {
+  const [isCurrActive, setIsCurrActive] = useState(false)
+
+  return (
+    <>
+        {!isCurrActive ?
+          <h3 onClick={()=> setIsCurrActive(!isCurrActive)} className='word-start'>{starter}</h3> :
+          <>
+          <h3 onClick={()=> setIsCurrActive(!isCurrActive)} className='word-start'>{starter}</h3>
+          {definitions.length ? definitions.map((defined: string, i: number) => (
+            <p key={i + 1}>{i + 1} • {defined}</p>
+            )) : null}
+            {synonyms.length ?
+              <p className='synonyms'>Synonyms: {synonyms.map((synonym: string) => (
+                <Link to={`/${synonym}`} key={synonym}>{synonym}</Link>
+                ))}</p>
+                : null}
+                {mp3Url ?
+                  <MyAudioPlayer mp3Url={mp3Url} />
+                  : null}
+                  </>
+        }
+    </>
+  )
+}
--- src/Components/Word/index.tsx
+++ src/Components/Word/index.tsx
@@ -1,32 +1,25 @@
 import React, { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom'
-import MyAudioPlayer from '../MyAudioPlayer'
-import { findUsableMp3, WordObj, WordInfo, Definitions } from '../../assets/functions'
+import { useParams } from 'react-router-dom'
+import { findUsableMp3, WordObj, WordInfo } from '../../assets/functions'
+import ActiveWord from '../ActiveWord'
 import './Word.css'
 
 export default function Word(): React.ReactElement {
   const { word } = useParams()
 
-  const [definitions, setDefinitions] = useState<Definitions[]>([])
-  const [partOfSpeech, setPartOfSpeech] = useState('')
-  const [synonyms, setSynonyms] = useState([])
   const [error, setError] = useState('')
-  const [sample, setSample] = useState('')
   const [wordInfo, setWordInfo] = useState<WordInfo[]>([])
   const [wordObj, setWordObj] = useState<WordObj[]>([])
-
-  console.log(wordInfo)
-  console.log(wordObj)
 
   useEffect(() => {
     setWordObj(
       wordInfo.flatMap(info => {
       return info.meanings.flatMap((meaning) => {
         return {
-        starter: `${word} • ${meaning.partOfSpeech}`,
+        starter: `${word?.toLowerCase()} • ${meaning.partOfSpeech} ${info.phonetic ? ' • ' + info.phonetic : ''}`,
         definitions: meaning.definitions.map(define => define.definition),
         synonyms: meaning.synonyms.map(synonym => synonym),
-        mp3Url: findUsableMp3(info.phonetics)
+        mp3Url: findUsableMp3(info.phonetics),
         }
       })
     })
@@ -41,13 +34,8 @@ export default function Word(): React.ReactElement {
       if (data.message) {
         setError(data.message)
       } else {
-        let url = findUsableMp3(data[0].phonetics)
         setError('')
-        setPartOfSpeech(data[0].meanings[0].partOfSpeech)
-        setSynonyms(data[0].meanings[0].synonyms)
-        setDefinitions(data[0].meanings[0].definitions)
         setWordInfo(data)
-        setSample(url)
       }
     })()
 
@@ -60,19 +48,15 @@ export default function Word(): React.ReactElement {
       <p className='error-layout'>Searched for {word?.toLowerCase()}: {error}</p>
       :
       <>
-      <h3 className='word-start'>{word?.toLowerCase()} • {partOfSpeech}</h3>
-          {definitions.length ? definitions.map((defined, i) => (
-            <p key={i + 1}>{i + 1} • {defined?.definition}</p>
-          )) : null}
-          {synonyms.length ?
-          <p className='synonyms'>Synonyms: {synonyms.map((synonym => (
-            <Link to={`/${synonym}`} key={synonym}>{synonym}</Link>
-            )))}</p>
-          : null}
-          {sample ?
-          <MyAudioPlayer mp3Url={sample} />
-          : null}
-        </>
+        {wordObj.length ? wordObj.map((currWord, i) => (
+          <ActiveWord key={`${i + 1}${word}`}
+          starter={currWord.starter}
+          definitions={currWord.definitions}
+          synonyms={currWord.synonyms}
+          mp3Url={currWord.mp3Url}
+          />
+        )) : null}
+      </>
        }
        <div className="created-by">
         <div>Created by: </div>
